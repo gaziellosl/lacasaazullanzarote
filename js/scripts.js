b534644f570f23d440eec4ef330ee1084219b8a7
Alexis Gaziello <alexisgaziello@MacBook-Air-de-Alexis.local>
2019-06-30 13:04:40 +0200

    SLIDES generalised + automatic

--- js/scripts.js
+++ js/scripts.js
@@ -9,38 +9,50 @@ function hamburgerMenu() {
 
 // Slides
 
-function showSlides(n, plus=) {
+function showSlides(n, id, to=null) {
   var i;
-  var n = plus;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length-1) {slideIndex = 0}
-  if (n < 0) {slideIndex = slides.length-1}
+  if (to!=null){slideIndex[n]=to;}
+  console.log(slideIndex[n]);
+  var slides = document.getElementsByClassName("slides_"+id);
+  var dots = document.getElementsByClassName("dot_"+id);
+  if (slideIndex[n] > slides.length-1) {slideIndex[n] = 0}
+  if (slideIndex[n] < 0) {slideIndex[n] = slides.length-1}
   for (i = 0; i < slides.length; i++) {
-      if (slides[i].style.display == "block"){
-        slides[i].style.display = "none";
-        slideIndex = slideIndex + i;
-        break;
-      }
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex].style.display = "block";
-  dots[slideIndex].className += " active";
+  slides[slideIndex[n]].style.display = "block";
+  dots[slideIndex[n]].className += " active";
 }
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(n);
+function showSlidesPlus(n, id, plus){
+   slideIndex[n]=slideIndex[n]+plus;
+   showSlides(n, id);
+ }
+
+ function showSlidesPlusWithCheck(n, id, plus){
+   if (!document.getElementById("slideshow-container_"+id).mouseIsOver){
+     showSlidesPlus(n, id, plus)
+   }
+  }
+
+var slideIndex = []
+
+function initDivMouseOver(id)   {
+   var div = document.getElementById("slideshow-container_"+id);
+   console.log("HELLO")
+   div.mouseIsOver = false;
+   div.onmouseover = function()   {
+      this.mouseIsOver = true;
+   };
+   div.onmouseout = function()   {
+      this.mouseIsOver = false;
+   }
+   // div.onclick = function()   {
+   //    if (this.mouseIsOver)   {
+   //       ....
+   //    }
+   // }
 }
-
-// Thumbnail image controls
-function currentSlide(n) {
-  var slideIndex = getSlidesIndex();
-  showSlides(slideIndex = n);
-}
-
-showSlides(0);
-
-// setTimeout(plusSlides(+1), 1000)
